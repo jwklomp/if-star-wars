983211d37186a8e69137cc763c2c8721793b3a55
jwklomp <jan-willem.klomp@visma.com>
2024-11-24 17:17:55 +0100

    format

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,36 +1,36 @@
-import * as React from "react";
-import {ReactNode} from "react";
-import {isEmpty, isNotEmpty} from "ramda";
-import {Else, If, Then} from "react-if";
+import { ReactNode } from "react";
+import { isEmpty, isNotEmpty } from "ramda";
+import { Else, If, Then } from "react-if";
 
 interface ListProps<T> {
-    empty: ReactNode;
-    header?: ReactNode;
-    items: Array<T>;
-    renderItem: (item: T) => ReactNode;
+  empty: ReactNode;
+  header?: ReactNode;
+  items: Array<T>;
+  renderItem: (item: T) => ReactNode;
 }
 
 /**
- * Abstracts the rendering of a list of items. It provides a way to render an optional header, a list of items, and a message when the list is empty.
+ * Abstract rendering of a list. Provides a way to render an optional header, a list of items, and empty list message.
  */
-const List = <T, >({
-      empty,
-      header,
-      items,
-      renderItem
+const List = <T,>({
+  empty,
+  header,
+  items,
+  renderItem,
 }: ListProps<T>): React.ReactElement => (
-
-    <If condition={isEmpty(items)}>
-        <Then>{empty}</Then>
-        <Else>
-            <If condition={isNotEmpty(header)}>{header}</If>
-            <ul className="list-group">
-                {items.map((item, index) => (
-                    <li className="list-group-item" key={index}>{renderItem(item)}</li>
-                ))}
-            </ul>
-        </Else>
-    </If>
+  <If condition={isEmpty(items)}>
+    <Then>{empty}</Then>
+    <Else>
+      <If condition={isNotEmpty(header)}>{header}</If>
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li className="list-group-item" key={index}>
+            {renderItem(item)}
+          </li>
+        ))}
+      </ul>
+    </Else>
+  </If>
 );
 
 export default List;
